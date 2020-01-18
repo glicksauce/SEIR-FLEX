4c53b4dfe8f57a6b9478328d6dc4eec2e5d02343
Jay Glickman <jayglickman@gmail.com>
2020-01-18 15:43:57 -0500

    adding seir flex to my personal repo

---
+++ unit_1/w06d2/student_labs/domdata/app.js
@@ -0,0 +1,31 @@
+$(() => {
+    console.log("up and running")
+
+    const data = [
+        { name: "Megatron", address: "Cybertron" },
+        { name: "Some guy", address: "Some street" },
+        { name: "Grace Hopper", address: "Arlington, Virginia" },
+        { name: "Ching Shih", address: "The High Seas" },
+        { name: "Slimer", address: "The Library" },
+        { name: "Umbra", address: "The Void" },
+        { name: "Hypatia", address: "The Neoplatonic school at Alexandria" },
+        { name: "Matt Huntington", address: "Remote" },
+        { name: "Ronald McDonald", address: "Casa del McDonalds" },
+        { name: "Jem", address: "Starlight Music" }
+      ];
+
+
+
+    $(()=>{
+        for (let i=0; i < data.length; i++) {
+            console.log(data[i]);
+            const $infoContainer = $('<div>').addClass("info-container")
+            const $nameDiv = $('<div>').addClass('name').text(data[i].name)
+            const $addressDiv = $('<div>').addClass('address').text(data[i].address)
+
+            $infoContainer.append($nameDiv)
+            $infoContainer.append($addressDiv)
+        }
+    });
+
+});
